e9a14fb57f4536bd94781db98f04c1338d476130
Tr1renity <157875286+Tr1renity@users.noreply.github.com>
2024-04-12 21:20:28 +0400

    Add files via upload

---
+++ banknote/banknote/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banknote
+{
+    internal static  class Class1
+    {
+        private static int[] denom = { 1, 2, 5, 10, 100,200, 1000, 2000, 5000, 10000 };
+        public static int  Sum(int numbills,int denom )
+        {
+            return denom * numbills;    
+        }
+        public static bool check(int arg)
+        {
+            return denom.Contains(arg);
+
+        }
+    }
+}
---
+++ banknote/banknote/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace banknote
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Class1.check(int.Parse(textBox1.Text)))
+            {
+                textBox3.Text = (Class1.Sum(int.Parse(textBox1.Text), int.Parse(textBox2.Text))).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Номинал указан неверно");
+            }
+        }
+    }
+}
